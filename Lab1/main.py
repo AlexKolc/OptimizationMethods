2564ea98800e32f8d8d07f1c719c5dc9e2279325
AlexKolc <alex_kolc@inbox.ru>
2019-10-20 05:38:31 +0300

    First commit

---
+++ Lab1/main.py
@@ -0,0 +1,26 @@
+import math
+
+#INITIALIZATION
+a = -math.pi / 2
+b = math.pi / 2
+
+
+def fx(x):
+    return math.sin(x)
+
+
+#PART I
+
+    def dichotomyMethod():
+        return 0
+
+    def goldenRatioMethod():
+        return
+
+    def fibonacciMethod():
+        return
+
+    def minFunctionOnLineSearch():
+        return
+
+#PART II
