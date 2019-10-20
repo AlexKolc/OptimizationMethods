efbd13fc1aa4506b67f5f7d98ac45e4be80cb92b
AlexKolc <alex_kolc@inbox.ru>
2019-10-20 12:10:22 +0300

    Relized fibonacci method

--- Lab1/part1.py
+++ Lab1/part1.py
@@ -4,14 +4,22 @@ import math
 # FUNCTION
 
 def f(x):
-    return math.pow(x + 5, 4)
-    #return math.sin(x)
+    #return math.pow((x - 15), 2) + 5
+    #return math.pow(x + 5, 4)
+    return math.sin(x)
+
+
+def F(n):
+    return 1 / math.sqrt(5) * (math.pow((1 + math.sqrt(5)) / 2, n)
+                               - math.pow((1 - math.sqrt(5)) / 2, n))
 
 
 # PART I
 
 def dichotomyMethod(a, b, eps):
+    iterations = 0
     while math.fabs(b - a) > eps:
+        iterations += 1
         x = (a + b) / 2
         f1 = f(x - eps)
         f2 = f(x + eps)
@@ -36,8 +44,29 @@ def goldenRatioMethod(a, b, eps):
     return f(x)
 
 
-def fibonacciMethod():
-    return
+def fibonacciMethod(a, b, eps):
+    n = math.ceil(math.log2(math.sqrt(5) * (b - a) / eps) / math.log2((1 + math.sqrt(5)) / 2)) - 2
+
+    # num = int(math.ceil(math.log((math.sqrt(5) * (b - a)) / (eps * (3 + math.sqrt(5))), (1 + math.sqrt(5)) / 2)))
+    #
+    # n_iter = 0
+    #
+    # while F(n_iter + 2) < (b - a) / eps:
+    #     n_iter += 1
+    #
+    # print(n, num, n_iter)
+
+    k = 1
+    while k < n:
+        x1 = a + F(n - k + 1) / F(n - k + 3) * (b - a)
+        x2 = a + F(n - k + 2) / F(n - k + 3) * (b - a)
+        if f(x1) < f(x2):
+            b = x2
+        else:
+            a = x1
+        k += 1
+    x = (a + b) / 2
+    return f(x)
 
 
 def minFunctionOnLineSearch():
@@ -46,13 +75,14 @@ def minFunctionOnLineSearch():
 
 def main():
     # INITIALIZATION
-    a, b = -10, 15
-    #a = -math.pi / 2
-    #b = math.pi / 2
+    #a, b = 2, 200
+    #a, b = -10, 15
+    a, b = -math.pi / 2, math.pi / 2
     eps = 1e-3
 
     print("Dichotomy method: ", dichotomyMethod(a, b, eps))
     print("Golden ratio method: ", goldenRatioMethod(a, b, eps))
+    print("Fibonacci method", fibonacciMethod(a, b, eps))
 
 
 if __name__ == '__main__':
